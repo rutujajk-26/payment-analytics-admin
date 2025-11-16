0c0dfe0237ed3a6557c8c7078e99b365fda4e7ae
rutujajk-26 <rutujakokane491@gmail.com>
2025-11-16 19:35:46 +0530

    fixed multipage build

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         operations: resolve(__dirname, 'public/operations/index.html'),
-        transaction: resolve(__dirname, 'public/transaction/index.html'),
         analytics: resolve(__dirname, 'public/analytics/index.html'),
+        transaction: resolve(__dirname, 'public/transaction/index.html'),
       },
     },
   },
