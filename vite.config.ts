a649aa1e54023a4cbd7e2b6800225371cfd8c051
rutujajk-26 <rutujakokane491@gmail.com>
2025-11-16 19:47:42 +0530

    Fixed Vite config + build working

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
   server: {
-    port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        operations: resolve(__dirname, 'public/operations/index.html'),
-        analytics: resolve(__dirname, 'public/analytics/index.html'),
-        transaction: resolve(__dirname, 'public/transaction/index.html'),
-      },
-    },
-  },
+    port: 3000
+  }
 })
