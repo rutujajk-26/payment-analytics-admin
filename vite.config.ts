4bd1c06bc511f667e77340c273c3d3ef38ec3149
rutujajk-26 <rutujakokane491@gmail.com>
2025-11-16 20:17:56 +0530

    Fix multipage routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,15 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  server: {
-    port: 3000,
-  },
+  server: { port: 3000 },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         operations: resolve(__dirname, 'public/operations/index.html'),
         transaction: resolve(__dirname, 'public/transaction/index.html'),
-        analytics: resolve(__dirname, 'public/analytics/index.html'),
-      },
-    },
-  },
+        analytics: resolve(__dirname, 'public/analytics/index.html')
+      }
+    }
+  }
 })
